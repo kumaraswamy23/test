866e3d814ba2424551fb0eda8b698074cf0547f0
kumaraswamy23 <kolla.kumaraswamy@gmail.com>
2023-05-17 07:52:19 +0530

    Add files via upload

---
+++ AttributeContract.java
@@ -0,0 +1,10 @@
+package com.ping.admin.api.selectormodel;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class AttributeContract {
+		
+    
+    
+}
